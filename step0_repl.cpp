275d279d8df1c3e915c50d3327350814a0430e76
dustinburda <dustin99wii@gmail.com>
2023-12-07 21:27:29 -0500

    Step 0 completed

--- step0_repl.cpp
+++ step0_repl.cpp
@@ -3,7 +3,37 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
+
+std::string READ (const std::string& in) {
+    auto ast = in;
+    return ast;
+}
+
+std::string EVAL(const std::string& ast) {
+    return ast;
+}
+
+std::string PRINT(const std::string& ast) {
+    auto out = ast;
+    return out;
+}
+
+std::string rep(const std::string& in) {
+    return PRINT(EVAL(READ(in)));
+}
 
 int main() {
-    std::cout << "Hello World\n" << std::endl;
+    while(true) {
+        std::cout << "user> ";
+
+        std::string line;
+        getline(std::cin, line);
+
+        if(std::cin.eof())
+            break;
+
+        std::cout << rep(line) << "\n";
+    }
 }
