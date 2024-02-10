0631aca633b3422b7efd82b1a4de957867e8c589
dustinburda <dustin99wii@gmail.com>
2024-02-10 17:04:20 -0500

    registered functions

--- Core.h
+++ Core.h
@@ -74,6 +74,52 @@ static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> divide = [](auto args) {
     return std::make_shared<MalNumber>(result);
 };
 
+//TODO implement this
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> list = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> is_list = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> is_empty = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> count = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> eq = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> l = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> le = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> g = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
+static std::function<mal_t_ptr(std::vector<mal_t_ptr>)> ge = [](auto args) {
+    mal_t_ptr s;
+    return s;
+};
+
 
 class Core {
 public:
@@ -84,6 +130,16 @@ public:
         ns_["-"] = std::make_shared<MalFunction>(minus);
         ns_["def!"] = std::make_shared<MalSymbol>("def!");
         ns_["let*"] = std::make_shared<MalSymbol>("let*");
+        ns_["list"] = std::make_shared<MalFunction>(list);
+        ns_["list?"] = std::make_shared<MalFunction>(is_list);
+        ns_["empty?"] = std::make_shared<MalFunction>(is_empty);
+        ns_["count"] = std::make_shared<MalFunction>(count);
+        ns_["="] = std::make_shared<MalFunction>(eq);
+        ns_["<"] = std::make_shared<MalFunction>(l);
+        ns_["<="] = std::make_shared<MalFunction>(le);
+        ns_[">"] = std::make_shared<MalFunction>(g);
+        ns_[">="] = std::make_shared<MalFunction>(ge);
+
     }
 
     const std::unordered_map<std::string, mal_t_ptr>& ns() const {
